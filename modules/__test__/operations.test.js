63ea96e2d7ffed777f523946cd6539a5bd89e973
mnuxD <mbaellav@uni.pe>
2022-05-02 19:39:09 -0500

    commit 4

--- modules/__test__/operations.test.js
+++ modules/__test__/operations.test.js
@@ -2,7 +2,7 @@ import { sum } from "../operations.js";
 
 describe("test operation module", () => {
   it("should solver a sum", () => {
-    const value = sum("1", "3");
+    const value = sum(1, 3);
     console.log(value, typeof value);
     expect(value).toBe(4);
   });
